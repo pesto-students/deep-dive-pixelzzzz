ebb0ea686371fab6bade780ab6255fece05512b1
pixelzzzz <kumar19900706@gmail.com>
2020-04-23 19:56:49 +0530

    sleep-promise-solution

---
+++ js-exercises/sleep-promise/sleepPromise.js
@@ -0,0 +1,10 @@
+function sleep(ms) {
+  const promise = new Promise(res => setTimeout(res, ms));
+  const innerFunction = (value) => promise.then(() => value);
+  innerFunction.then = promise.then.bind(promise);
+  innerFunction.catch = promise.catch.bind(promise);
+  return innerFunction;
+}
+export {
+  sleep,
+};
