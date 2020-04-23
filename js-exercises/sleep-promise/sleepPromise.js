3c9b9a308e76c3eafb8dfb9f360598363210d3f1
pixelzzzz <kumar19900706@gmail.com>
2020-04-23 16:16:31 +0530

    indention completed

--- js-exercises/sleep-promise/sleepPromise.js
+++ js-exercises/sleep-promise/sleepPromise.js
@@ -1,11 +1,13 @@
-function sleep(ms){
-    const promise = new Promise(res=>setTimeout(res,ms));
-    const innerFunction=(value)=>{
-       return promise.then(()=>value)
-    }
-    innerFunction.then=promise.then.bind(promise);
-    innerFunction.catch=promise.catch.bind (promise);
-    return innerFunction;
+function sleep(ms) {
+	const promise = new Promise(res => setTimeout(res, ms));
+	const innerFunction = (value) => {
+		return promise.then(() => value)
+	}
+	innerFunction.then = promise.then.bind(promise);
+	innerFunction.catch = promise.catch.bind(promise);
+	return innerFunction;
 }
-export { sleep };
+export {
+	sleep
+};
 
