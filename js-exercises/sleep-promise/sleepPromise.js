c81ca8c7f28d218961e0616223a709e4ff09bf53
pixelzzzz <kumar19900706@gmail.com>
2020-04-23 15:28:45 +0530

    sleep-promise-formatted

--- js-exercises/sleep-promise/sleepPromise.js
+++ js-exercises/sleep-promise/sleepPromise.js
@@ -1,10 +1,10 @@
-function sleep(ms) {
+function sleep(ms){
     const promise = new Promise(res=>setTimeout(res,ms));
     const innerFunction=(value)=>{
        return promise.then(()=>value)
     }
     innerFunction.then=promise.then.bind(promise);
-    innerFunction.catch=promise.catch.bind(promise);
+    innerFunction.catch=promise.catch.bind (promise);
     return innerFunction;
 }
 export { sleep };
