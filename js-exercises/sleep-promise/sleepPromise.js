7317bc590ca1d034726f5e56a7d51a22bf10caa7
pixelzzzz <kumar19900706@gmail.com>
2020-04-23 13:47:39 +0530

    ignore eslint

---
+++ js-exercises/sleep-promise/sleepPromise.js
@@ -0,0 +1,11 @@
+function sleep(ms) {
+    const promise = new Promise(res=>setTimeout(res,ms));
+    const innerFunction=(value)=>{
+       return promise.then(()=>value)
+    }
+    innerFunction.then=promise.then.bind(promise);
+    innerFunction.catch=promise.catch.bind(promise);
+    return innerFunction;
+}
+export { sleep };
+
