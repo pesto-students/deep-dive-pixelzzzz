fe076bbba25f458bf03cb38447ef3766754dced3
pixelzzzz <kumar19900706@gmail.com>
2020-05-09 19:08:50 +0530

    added skipping of commented lines

--- node-exercises/csv-parser-node/index.js
+++ node-exercises/csv-parser-node/index.js
@@ -3,12 +3,12 @@ const parse = require("./parse");
 
 let server = restify.createServer();
 server.listen(process.env.port || 3000, function () {
-  parse.parseCSV("./example.csv", {
-    seperator: ",",
-    headers: false,
-    modifyheader: (headers) => headers.map((header) => header.toUpperCase()),
-  });
-  //parse.parseCSV("./example.csv", { seperator: ",", headers: true });
-  // parse.parseCSV("./file.csv", { seperator: ";", headers: true });
+  parse
+    .parseCSV("./small.csv", {
+      headers: false,
+    })
+    .then((data) => console.log(data));
+  //parse.parseCSV("./example.csv", { headers: true });
+  // parse.parseCSV("./file.csv", { headers: true });
   console.log("server is running");
 });
--- node-exercises/csv-parser-node/parse.js
+++ node-exercises/csv-parser-node/parse.js
@@ -1,39 +1,50 @@
 const fs = require("fs");
 exports.parseCSV = function (filePath, options) {
-  const readStream = fs.createReadStream(filePath);
-  let fileToBeRead = "";
+  return new Promise((res, rej) => {
+    const readStream = fs.createReadStream(filePath);
+    let fileToBeRead = "";
 
-  readStream.on("data", (chunk) => (fileToBeRead += chunk));
+    readStream.on("data", (chunk) => (fileToBeRead += chunk));
 
-  readStream.on("end", () => {
-    if (options.headers) {
-      let fileToBeReadArray = fileToBeRead.split("\r\n");
-      let keys = fileToBeReadArray.shift().split(options.seperator);
-      if (options.modifyheader) {
-        keys = options.modifyheader(keys);
-      }
-      const jsonData = fileToBeReadArray.map((firstLine) => {
-        const subData = firstLine.split(options.seperator);
-        const obj = {};
-        for (let i = 0; i < keys.length; i += 1) {
-          obj[keys[i]] = subData[i];
+    readStream.on("end", () => {
+      if (options.headers) {
+        let keys;
+        const fileToBeReadArray = fileToBeRead.split("\r\n");
+        if (fileToBeReadArray[0][0] === "#") {
+          fileToBeReadArray.shift(); //remove the commented line
+          keys = fileToBeReadArray.shift().split(",");
+        } else {
+          keys = fileToBeReadArray.shift().split(",");
         }
-        return obj;
-      });
-      console.log(jsonData);
-    } else {
-      let arrayData = [];
-      const fileToBeReadArray = fileToBeRead.split("\r\n");
-      fileToBeReadArray.forEach((line) => {
-        let eachLineArray = [];
-        let splitLine = line.split(options.seperator);
-        splitLine.forEach((line) => eachLineArray.push(line));
-        arrayData.push(eachLineArray);
-      });
-      console.log(arrayData);
-    }
-  });
-  readStream.on("close", () => console.log("stream closed"));
+        if (fileToBeReadArray[fileToBeReadArray.length - 1][0] === "#") {
+          fileToBeReadArray.pop();
+        }
+        if (options.modifyheader) {
+          keys = options.modifyheader(keys);
+        }
+        const jsonData = fileToBeReadArray.map((line) => {
+          const subData = line.split(",");
+          const obj = {};
+          for (let i = 0; i < keys.length; i += 1) {
+            obj[keys[i]] = subData[i];
+          }
+          return obj;
+        });
+        res(jsonData);
+      } else {
+        let arrayData = [];
+        const fileToBeReadArray = fileToBeRead.split("\r\n");
+        fileToBeReadArray.forEach((line) => {
+          let eachLineArray = [];
+          let splitLine = line.split(",");
+          splitLine.forEach((line) => eachLineArray.push(line));
+          arrayData.push(eachLineArray);
+        });
+        res(arrayData);
+      }
+    });
+    readStream.on("close", () => console.log("stream closed"));
 
-  readStream.on("error", (error) => reject(error));
+    readStream.on("error", (error) => reject(error));
+  });
 };
